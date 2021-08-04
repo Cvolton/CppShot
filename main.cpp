1b7e9c5a278b75277de1e2e63b5ec9dd257cb80b
Cvolton <cvolton@cvolton.eu>
2021-08-04 14:55:44 +0200

    Added version number into the window title

--- main.cpp
+++ main.cpp
@@ -402,7 +402,7 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     hwnd = CreateWindowEx (
            0,                   /* Extended possibilites for variation */
            szClassName,         /* Classname */
-           _T("CppShot"),       /* Title Text */
+           _T("CppShot 0.2"),       /* Title Text */
            WS_OVERLAPPEDWINDOW, /* default window */
            CW_USEDEFAULT,       /* Windows decides the position */
            CW_USEDEFAULT,       /* where the window ends up on the screen */
